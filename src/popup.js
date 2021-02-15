cf82ce1a01cb9fc7e9dbc8dca270f3c49619ac2c
Iain <ialexanderb@outlook.com>
2021-02-15 20:52:20 +0000

    Added Small Features and Commented Code
    Added the link to Github Repository via button, the Password Reminder displaying the date on which password should be changed and a simple user input function which just displays the user's password and saves it to a variable. Also added comments to the HTML and Javascript files.

--- src/popup.js
+++ src/popup.js
@@ -1,82 +1,158 @@
-window.onload = function() {
+//Set-Up Function. Hides everything but the Password Strength Page & Components on the Extension being opened.
+window.onload = function() 
+{
     passEtiqPage.style.display = "none";
-	passStrengthPage.style.display = "block";
 	threeRandom.style.display = "none";
 	generalTips.style.display = "none";
 	extraSecurity.style.display = "none";
+	passStrengthPage.style.display = "block";
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-
+//Button Listening Function. Listens for "Input Button" clicks and calls the relevant function when they are registered.
+document.addEventListener('DOMContentLoaded', function () 
+{
+	//Setting up the variables.
 	var passPageButton = document.getElementById("passPageButton");
 	var threeWordsButton = document.getElementById("threeWordsButton");
 	var generalTipsButton = document.getElementById("generalTipsButton");
 	var extraSecurityButton = document.getElementById("extraSecurityButton");
+	var repoLinkButton = document.getElementById("repoLinkButton");
+	var passCheckButton = document.getElementById("passCheckButton");
 	
-    passPageButton.addEventListener('click', function(){
+	//Adding each of the "Input Button Listeners"
+    passPageButton.addEventListener('click', function()
+	{
+		//Runs the display function for switching between the Password Strength Checker and Password Etiquette "Pages"
 		passPageToggle();
 	});
-	threeWordsButton.addEventListener('click', function(){
+	threeWordsButton.addEventListener('click', function()
+	{
+		//Runs the display function for the Three Random Words Division
 		threeWordsToggle();
 	});
-	generalTipsButton.addEventListener('click', function(){
+	generalTipsButton.addEventListener('click', function()
+	{
+		//Runs the display function for the General Tips Division
 		generalTipsToggle();
 	});
-	extraSecurityButton.addEventListener('click', function(){
+	extraSecurityButton.addEventListener('click', function()
+	{
+		//Runs the display function for the Extra Security Division
 		extraSecurityToggle();
+	});
+	repoLinkButton.addEventListener('click', function()
+	{
+		//Runs a function providing a link to the Github Repository
+		openRepoLink();
+	});
+	passCheckButton.addEventListener('click', function()
+	{
+		//Runs the Password Checking Function
+		passCheck();
 	});
 });
 
-function passPageToggle() {
+//PassCheck Function
+//Still needs checking user input against breached passwords file and the Security Consultant
+function passCheck() 
+{
+	document.getElementById("passwordTitle").innerHTML = "Your Password is: " + document.getElementById("passForm").value;
+	var password = document.getElementById("passForm").value;
+	
+	var d = new Date().getDate();
+	var m = new Date().getMonth() + 1;
+	var y = new Date().getFullYear();
+	document.getElementById("date").innerHTML = "We recommend that you change it by: " + d + "/" + (m + 6) + "/"  + y;
+};
+
+//OpenRepoLink Function
+function openRepoLink() 
+{
+		//Opens the supplied URL in a new tab
+	    var newURL = "https://github.com/Team15-EH/Password-Extension-Project";
+        chrome.tabs.create({ url: newURL });
+}
+
+function passPageToggle() 
+{
+	//Gets both the passStrengthPage and passEtiqPage Divisions and assigns them to variables 
 	var x = document.getElementById("passStrengthPage");
 	var y = document.getElementById("passEtiqPage");
 	
-	if (x.style.display === "none") {
+	//Checks if on the passEtiqPage and if so switches display components to the passStrengthPage 
+	if (x.style.display === "none") 
+	{
+		//Makes the "Check Password" button visible
 		passCheckButton.style.visibility = "visible";
+		//Hides passEtiqPage and displays passStrengthPage
 		y.style.display = "none";
 		x.style.display = "block";
+		//Hides the passEtiqPage components
+		threeRandom.style.display = "none";
+		generalTips.style.display = "none";
+		extraSecurity.style.display = "none";
+		//Displays the enter password form
 		passForm.style.display = "block";
-	} else {
+	} else 
+	{
+		//Changes the passPageButton (Used to toggle the page) to say "Back to Main Page"
 		document.getElementById("passPageButton").value = "Back to Main Page";
+		//Make the "Check Password" button dissapear but keep its position in the Division
 		passCheckButton.style.visibility = "hidden";
+		//Display the password etiquette security summary
 		passSecuritySummary.style.display = "block";
+		//Hide the passCheck page and display the passEtiqPage
 		x.style.display = "none";
 		y.style.display = "block";
+		//Make the enter password form dissapear 
 		passForm.style.display = "none";
 	}
 };
 
-function threeWordsToggle() {
+//Three Random Words Function
+function threeWordsToggle()
+{
+	//Hide password etiquette summary
 	passSecuritySummary.style.display = "none";
 	
+	//Assign passEtiqPage components to variables
 	var x = document.getElementById("threeRandom");
 	var y = document.getElementById("generalTips");
 	var z = document.getElementById("extraSecurity");
 	
+	//Display the threeRandomWords component and hide the others
 	x.style.display = "block";
 	y.style.display = "none";
 	z.style.display = "none";
 };
 
-function generalTipsToggle() {
+function generalTipsToggle() 
+{
+	//Hide password etiquette summary
 	passSecuritySummary.style.display = "none";
 	
+	//Assign passEtiqPage components to variables
 	var x = document.getElementById("threeRandom");
 	var y = document.getElementById("generalTips");
 	var z = document.getElementById("extraSecurity");
 	
+	//Display the generalTips component and hide the others
 	x.style.display = "none";
 	y.style.display = "block";
 	z.style.display = "none";
 };
 
-function extraSecurityToggle() {
+function extraSecurityToggle() 
+{
+	//Hide password etiquette summary
 	passSecuritySummary.style.display = "none";
 	
+	//Assign passEtiqPage components to variables
 	var x = document.getElementById("threeRandom");
 	var y = document.getElementById("generalTips");
 	var z = document.getElementById("extraSecurity");
 
+	//Display the Extra Security component and hide the others
 	x.style.display = "none";
 	y.style.display = "none";
 	z.style.display = "block";
