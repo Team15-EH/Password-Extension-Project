900f1a9d837cf9406de491916d4362ff2deac004
Luke Morgan <Lukemorgan1956@hotmail.com>
2021-02-22 14:52:16 +0000

    Changed the password strength check
    Changed the function to include special characters and how it determines whether a password is strong

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,7 @@
 //Set-Up Function. Hides everything but the Password Strength Page & Components on the Extension being opened.
-window.onload = function() 
+window.onload = function()
 {
-    passEtiqPage.style.display = "none";
+  passEtiqPage.style.display = "none";
 	threeRandom.style.display = "none";
 	generalTips.style.display = "none";
 	extraSecurity.style.display = "none";
@@ -9,7 +9,7 @@ window.onload = function()
 }
 
 //Button Listening Function. Listens for "Input Button" clicks and calls the relevant function when they are registered.
-document.addEventListener('DOMContentLoaded', function () 
+document.addEventListener('DOMContentLoaded', function ()
 {
 	//Setting up the variables.
 	var passPageButton = document.getElementById("passPageButton");
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function ()
 	var extraSecurityButton = document.getElementById("extraSecurityButton");
 	var repoLinkButton = document.getElementById("repoLinkButton");
 	var passCheckButton = document.getElementById("passCheckButton");
-	
+
 	//Adding each of the "Input Button Listeners"
     passPageButton.addEventListener('click', function()
 	{
@@ -51,28 +51,36 @@ document.addEventListener('DOMContentLoaded', function ()
 		passCheck();
 	});
 });
-
+const LOWER_REGEX = /([a-z])/g;
+const UPPER_REGEX = /([A-Z])/g;
+const NUM_REGEX = /([/d])/g;
+const SPECIAL_REGEX = /([$&+,:;=?@#|'<>.^*()%!-])/g
+var lowerMinCount = 2;
+var upperMinCount = 2;
+var numMinCount = 2;
+var specialMinCount = 2;
+var passwordMinLength = 8;
 //PassCheck Function
 //Still needs checking user input against breached passwords file and the Security Consultant
-function passCheck() 
+function passCheck()
 {
 	document.getElementById("passwordTitle").innerHTML = "Your Password is: " + document.getElementById("passForm").value;
 	var password = document.getElementById("passForm").value;
-	
+
 	var d = new Date().getDate();
 	var m = new Date().getMonth() + 1;
 	var y = new Date().getFullYear();
 	document.getElementById("date").innerHTML = "We recommend that you change it by: " + d + "/" + (m + 6) + "/"  + y;
-	
-	
-	
+
+
+
 	// Disect password to see what it contains
 	if (password.length > 0)
 	{
-	var numOfCapitals = (password.match(/[A-Z]/g) || []).length;
-	var numOfLower= (password.match(/[a-z]/g) || []).length;
-	var numOfNumbers = (password.match(/[0-9]/g) || []).length;
-	
+	var numOfCapitals = (password.match(UPPER_REGEX) || []).length;
+	var numOfLower= (password.match(LOWER_REGEX) || []).length;
+	var numOfNumbers = (password.match(NUM_REGEX) || []).length;
+  var numOfSpecial = (password.match(SPECIAL_REGEX) || []).length;
 	/*
 	//var numOfConsecutiveChars = (password.match(/[a-z]\1/ig) || []).length;
 	//var numOfSpecial = (password.match(/[@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g ).length;
@@ -82,9 +90,10 @@ function passCheck()
 	document.getElementById("number").innerHTML = " Number of Numbers  = " + numOfNumbers;
 	document.getElementById("upper").innerHTML = " Number of uppercase = " + numOfCapitals;
 	document.getElementById("lower").innerHTML = " Number of lowercase = " + numOfLower;
-	
+  document.getElementById("special").innerHTML = " Number of special = " + numOfSpecial;
+
 	/* Not working yet, possibly next iteration
-	
+
 	//document.getElementById("consecutive").innerHTML = " Number of consecutive characters = " + numOfConsecutiveChars;
 	//document.getElementById("special").innerHTML = " Number of special = " + numOfSpecial;
 	*/
@@ -94,47 +103,37 @@ function passCheck()
 	//failsafe
 	alert("Empty User Input, Please try again");
 	}
-	
-	
+
 	// set initial requirements that determine the strength of the password (will be tweaked)
-	if (password.length > 10 && numOfCapitals >=3 & numOfLower >=3 & numOfNumbers >=3 )
+	if ((password.length > passwordMinLength) && (numOfCapitals >= upperMinCount) & (numOfLower >= lowerMinCount) & (numOfNumbers >= numMinCount) & (numOfSpecial >= specialMinCount))
 	{
-	document.getElementById("onceCalc").innerHTML = "Your Password is: Strong";
+			document.getElementById("onceCalc").innerHTML = "Your Password is: Medium";
 	}
-	else if (password.length < 8 && numOfCapitals <2 & numOfLower <2 & numOfNumbers <2 )
+	else if ((password.length > (passwordMinLength + 2)) && (numOfCapitals >= (upperMinCount + 1)) & (numOfLower >= (lowerMinCount + 1)) & (numOfNumbers >= (numMinCount + 1)) & (numOfSpecial >= (specialMinCount + 1)))
 	{
-		document.getElementById("onceCalc").innerHTML = "Your Password is: Weak";
+		document.getElementById("onceCalc").innerHTML = "Your Password is: Strong";
 	}
-	else if (password.length > 8 && numOfCapitals >=2 & numOfLower >=2 & numOfNumbers >=2 )
-	{
-		document.getElementById("onceCalc").innerHTML = "Your Password is: Medium";
-	}
-	
-	
-	
-	
-	
-	
-	
-	
+  else {
+    document.getElementById("onceCalc").innerHTML = "Your Password is: Weak";
+  }
 };
 
 //OpenRepoLink Function
-function openRepoLink() 
+function openRepoLink()
 {
 		//Opens the supplied URL in a new tab
 	    var newURL = "https://github.com/Team15-EH/Password-Extension-Project";
         chrome.tabs.create({ url: newURL });
 }
 
-function passPageToggle() 
+function passPageToggle()
 {
-	//Gets both the passStrengthPage and passEtiqPage Divisions and assigns them to variables 
+	//Gets both the passStrengthPage and passEtiqPage Divisions and assigns them to variables
 	var x = document.getElementById("passStrengthPage");
 	var y = document.getElementById("passEtiqPage");
-	
-	//Checks if on the passEtiqPage and if so switches display components to the passStrengthPage 
-	if (x.style.display === "none") 
+
+	//Checks if on the passEtiqPage and if so switches display components to the passStrengthPage
+	if (x.style.display === "none")
 	{
 		//Makes the "Check Password" button visible
 		passCheckButton.style.visibility = "visible";
@@ -147,7 +146,7 @@ function passPageToggle()
 		extraSecurity.style.display = "none";
 		//Displays the enter password form
 		passForm.style.display = "block";
-	} else 
+	} else
 	{
 		//Changes the passPageButton (Used to toggle the page) to say "Back to Main Page"
 		document.getElementById("passPageButton").value = "Back to Main Page";
@@ -158,7 +157,7 @@ function passPageToggle()
 		//Hide the passCheck page and display the passEtiqPage
 		x.style.display = "none";
 		y.style.display = "block";
-		//Make the enter password form dissapear 
+		//Make the enter password form dissapear
 		passForm.style.display = "none";
 	}
 };
@@ -168,39 +167,39 @@ function threeWordsToggle()
 {
 	//Hide password etiquette summary
 	passSecuritySummary.style.display = "none";
-	
+
 	//Assign passEtiqPage components to variables
 	var x = document.getElementById("threeRandom");
 	var y = document.getElementById("generalTips");
 	var z = document.getElementById("extraSecurity");
-	
+
 	//Display the threeRandomWords component and hide the others
 	x.style.display = "block";
 	y.style.display = "none";
 	z.style.display = "none";
 };
 
-function generalTipsToggle() 
+function generalTipsToggle()
 {
 	//Hide password etiquette summary
 	passSecuritySummary.style.display = "none";
-	
+
 	//Assign passEtiqPage components to variables
 	var x = document.getElementById("threeRandom");
 	var y = document.getElementById("generalTips");
 	var z = document.getElementById("extraSecurity");
-	
+
 	//Display the generalTips component and hide the others
 	x.style.display = "none";
 	y.style.display = "block";
 	z.style.display = "none";
 };
 
-function extraSecurityToggle() 
+function extraSecurityToggle()
 {
 	//Hide password etiquette summary
 	passSecuritySummary.style.display = "none";
-	
+
 	//Assign passEtiqPage components to variables
 	var x = document.getElementById("threeRandom");
 	var y = document.getElementById("generalTips");
@@ -211,4 +210,3 @@ function extraSecurityToggle()
 	y.style.display = "none";
 	z.style.display = "block";
 };
-
